8c060e54845f6a987d937f64d14c1041da4b505a
Rafael Rodrigues <rafaelbrk75@gmail.com>
2025-10-08 17:21:32 -0300

    update

--- src/academy/devdojo/maratona/javacore/Kenum/dominio/TipoCliente.java
+++ src/academy/devdojo/maratona/javacore/Kenum/dominio/TipoCliente.java
@@ -1,6 +1,27 @@
 package academy.devdojo.maratona.javacore.Kenum.dominio;
 
 public enum TipoCliente {
-    PESSOA_FISICA,
-    PESSOA_JURIDICA
+    PESSOA_FISICA(1, "Pessoa Fisica"),
+    PESSOA_JURIDICA(2, "Pessoa Juridica");
+
+    public final int VALOR;
+    private String nomeRelatorio;
+
+    TipoCliente(int VALOR, String nomeRelatorio) {
+        this.VALOR = VALOR;
+        this.nomeRelatorio = nomeRelatorio;
+    }
+
+    public static TipoCliente tipoClientePorNomeRelatorio(String nomeRelatorio) {
+        for (TipoCliente tipoCliente : values()) {
+            if(tipoCliente.getNomeRelatorio().equals(nomeRelatorio)){
+                return tipoCliente;
+            }
+        }
+        return null;
+    }
+
+    public String getNomeRelatorio() {
+        return nomeRelatorio;
+    }
 }
--- src/academy/devdojo/maratona/javacore/Kenum/test/ClienteTest01.java
+++ src/academy/devdojo/maratona/javacore/Kenum/test/ClienteTest01.java
@@ -2,16 +2,19 @@ package academy.devdojo.maratona.javacore.Kenum.test;
 
 import academy.devdojo.maratona.javacore.Kenum.dominio.Cliente;
 import academy.devdojo.maratona.javacore.Kenum.dominio.TipoCliente;
+import academy.devdojo.maratona.javacore.Kenum.dominio.TipoPagamento;
 
 public class ClienteTest01 {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Rafael", TipoCliente.PESSOA_FISICA);
-        Cliente cliente2 = new Cliente("Rafael", TipoCliente.PESSOA_FISICA);
-        Cliente cliente3 = new Cliente("Rafael", TipoCliente.PESSOA_JURIDICA);
-        Cliente cliente4 = new Cliente("Rafael", TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente1 = new Cliente("Rafael", TipoCliente.PESSOA_FISICA, TipoPagamento.DEBITO);
+        Cliente cliente2 = new Cliente("Akira", TipoCliente.PESSOA_JURIDICA,  TipoPagamento.CREDITO);
+
         System.out.println(cliente1);
         System.out.println(cliente2);
-        System.out.println(cliente3);
-        System.out.println(cliente4);
+        System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
+        TipoCliente tipoCliente = TipoCliente.valueOf("PESSOA_FISICA");
+        System.out.println(tipoCliente);
+        TipoCliente tipoCliente2 = TipoCliente.tipoClientePorNomeRelatorio("Pessoa Fisica");
+        System.out.println(tipoCliente2);
     }
 }
